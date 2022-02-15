91b226ba3d51b7e3037236ea670d30f7844929be
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-15 11:29:55 -0800

    wrote middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,87 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model')
+var vinValidator = require('vin-validator')
+const db = require('../../data/db-config')
+
+const checkCarId = async (req, res, next) => {
+  const { id } = req.params
+  try {
+    const car = await Cars.getById(id)
+    if (!car) {
+      next({
+        status: 404,
+        message: `car with id ${id} is not found`
+      })
+    } else {
+      req.car = car
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { vin, make, model, mileage } = req.body;
+  if (!vin) {
+    next({
+      status: 400,
+      message: 'vin is missing'
+    })
+  } else if (!make) {
+    next({
+      status: 400,
+      message: 'make is missing'
+    })
+  } else if (!model) {
+    next({
+      status: 400,
+      message: 'model is missing'
+    })
+  } else if (!mileage) {
+    next({
+      status: 400,
+      message: 'mileage is missing'
+    })
+  } else {
+    next()
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { vin } = req.body;
+  if (vinValidator.validate(vin)) {
+    next()
+  } else {
+    next({
+      status: 400,
+      message: `vin ${vin} is invalid`
+    })
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  const { vin } = req.body;
+  try {
+    const existing = await db('cars')
+      .where({ vin: vin.trim() })
+      .first()
+
+    if (existing) {
+      next({
+        status: 400,
+        message: `vin ${vin} already exists`
+      })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,4 +1,14 @@
 const router = require('express').Router()
 const Cars = require('./cars-model')
+const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware')
+
+
+
+router.use((err, req, res, next) => {// eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
+})
 
 module.exports = router
