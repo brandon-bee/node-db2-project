baabe8203629c9c107f8cfc38f1108b2bd8b5dc0
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-15 11:38:50 -0800

    fixed server file and created first GET router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,7 +2,13 @@ const router = require('express').Router()
 const Cars = require('./cars-model')
 const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware')
 
-
+router.get('/', (req, res, next) => {
+  Cars.getAll()
+    .then(cars => {
+      res.json(cars)
+    })
+    .catch(next)
+})
 
 router.use((err, req, res, next) => {// eslint-disable-line
   res.status(err.status || 500).json({
